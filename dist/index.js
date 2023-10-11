aa36334e4e7566c30601b6b144834215cfaab90a
Masashi Fujita <masashifujita536@gmail.com>
2023-10-12 02:36:11 +0900

    persist docker volume

--- dist/index.js
+++ dist/index.js
@@ -1,3 +1,12 @@
 "use strict";
-const message = "Hello, world";
-console.log(message);
+const double = (num) => {
+    if (num === undefined) {
+        return 0;
+    }
+    else {
+        return num * 2;
+    }
+};
+console.log(double(undefined));
+console.log(double(123));
+console.log("aa".repeat(2));
--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,12 @@
-const message: string = "Hello, world";
+const double = (num: number | undefined) => {
+  if(num === undefined){
+    return 0
+  }else{
+    return num * 2
+  }
+}
 
-console.log(message);
+console.log(double(undefined));
+console.log(double(123))
+
+console.log("aa".repeat(2))
