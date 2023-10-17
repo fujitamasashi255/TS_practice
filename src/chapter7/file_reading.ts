7c1b3f022c87c5b000a2bc35342c09524e620112
Masashi Fujita <masashifujita536@gmail.com>
2023-10-17 21:31:50 +0900

    chapter7

---
+++ src/chapter7/file_reading.ts
@@ -0,0 +1,17 @@
+import fs from 'fs';
+import url from "node:url";
+import path from "node:path";
+
+const regex = /uhyo/g
+const source_path = url.fileURLToPath(import.meta.url)
+const target_path = `${path.dirname(source_path)}/../../doc/uhyo.txt`
+fs.readFile(target_path, 'utf8', (err, data) => {
+  if(err){
+    console.log(err)
+  }else{
+    const uhyo_count = data.match(regex)?.length
+    console.log(uhyo_count)
+  }
+})
+
+console.log(url.fileURLToPath(import.meta.url))
