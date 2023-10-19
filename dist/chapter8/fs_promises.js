ce57b897e507827d1b3ce66130b04119a07a9859
Masashi Fujita <masashifujita536@gmail.com>
2023-10-20 03:11:32 +0900

    chapter8 promise race

--- dist/chapter8/fs_promises.js
+++ dist/chapter8/fs_promises.js
@@ -1,10 +1,32 @@
 import { readFile } from "fs/promises";
 import { uhyoFilePath } from "../helper/file_helper.js";
-const regex = /uhyo/g;
-const pro = readFile(uhyoFilePath, 'utf8');
-pro.then((content) => {
-    const uhyo_count = content.match(regex)?.length;
-    console.log(uhyo_count);
-}).catch((err) => {
+const readfile_pro = () => {
+    return readFile(uhyoFilePath, 'utf8');
+};
+const timeout_pro = () => {
+    return new Promise((resolve, reject) => {
+        setTimeout(() => {
+            reject(new Error("1ms経過"));
+        }, 1);
+    });
+};
+/*
+上記関数は次でもOK
+const timeout_pro = async () => {
+  await setTimeout(() => {}, 1)
+  throw new Error
+}
+*/
+const content = "";
+try {
+    const content = await Promise.race([readfile_pro(), timeout_pro()]);
+}
+catch (err) {
     console.log(err);
-});
+}
+const regex = /uhyo/g;
+console.log(`読み込み結果：${content}`);
+console.log([].length);
+console.log(content.match(regex));
+const uhyo_count = content.match(regex)?.length;
+console.log(uhyo_count);
--- src/chapter8/fs_promises.ts
+++ src/chapter8/fs_promises.ts
@@ -2,12 +2,38 @@ import { readFile } from "fs/promises"
 import path from "path"
 import { uhyoFilePath } from "../helper/file_helper.js"
 
-const regex = /uhyo/g
 
-const pro: Promise<string> = readFile(uhyoFilePath, 'utf8')
-pro.then((content: string) => {
-  const uhyo_count = content.match(regex)?.length
-  console.log(uhyo_count)
-}).catch((err) => {
+const readfile_pro = (): Promise<string> => {
+  return readFile(uhyoFilePath, 'utf8')
+}
+
+const timeout_pro = () => {
+  return new Promise<unknown>((resolve, reject) => {
+    setTimeout(() => {
+      reject(new Error("1ms経過"))
+    }, 1)
+  })
+}
+
+/*
+上記関数は次でもOK
+const timeout_pro = async () => {
+  await setTimeout(() => {}, 1)
+  throw new Error
+}
+*/
+
+const content: string = ""
+try {
+  const content = await Promise.race([readfile_pro(), timeout_pro()])
+} catch(err) {
   console.log(err)
-})
+}
+
+const regex = /uhyo/g
+console.log(`読み込み結果：${content}`)
+console.log([].length)
+console.log(content.match(regex))
+const uhyo_count = content.match(regex)?.length
+console.log(uhyo_count)
+
